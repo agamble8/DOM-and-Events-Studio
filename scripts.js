c3675a3f9084637006b47424aa04fe57e33a0f10
Amy Gamble <amymgambl@gmail.com>
2020-08-25 23:38:09 -0500

    html and js working but without bonus feature 2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,96 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+let takeoffButton = null;
+let landButton = null;
+let abortButton = null;
+let upButton = null;
+let downButton = null;
+let leftButton = null;
+let rightButton = null;
+let flightStatus = null;
+let shuttleHeight = null;
+let backgroundCol = null;
+let rocket = null;
+let movement = null;
+
+function init () {
+    takeoffButton = document.getElementById("takeoff");
+    landButton = document.getElementById("landing");
+    abortButton = document.getElementById("missionAbort");
+    upButton = document.getElementById("up");
+    downButton = document.getElementById("down");
+    leftButton = document.getElementById("left");
+    rightButton = document.getElementById("right");
+    abortButton = document.getElementById("missionAbort");
+    flightStatus = document.getElementById("flightStatus");
+    shuttleHeight = document.getElementById("spaceShuttleHeight");
+    backgroundCol = document.getElementById("shuttleBackground");
+    rocket = document.getElementById("rocket");
+    rocket.style.position = 'absolute';
+    rocket.style.left = "0px";
+    rocket.style.bottom = "0px";
+
+
+    takeoffButton.addEventListener('click', function() {
+        let result = window.confirm('Confirm that the shuttle is ready for takeoff.');
+
+        if(result){
+            flightStatus.innerHTML = 'Shuttle in flight.';
+            backgroundCol.style.backgroundColor = 'blue';
+            shuttleHeight.innerHTML = 10000;
+        }
+    })
+
+    landButton.addEventListener('click', function() {
+        window.alert('The shuttle is landing. Landing gear engaged.');
+        flightStatus.innerHTML = 'The shuttle has landed.';
+        backgroundCol.style.backgroundColor = 'green';
+        shuttleHeight.innerHTML = 0;
+    })
+
+    abortButton.addEventListener('click', function() {
+        let result = window.confirm('Confirm that you want to abort the mission.');
+        if(result){
+            flightStatus.innerHTML = 'Mission aborted.';
+            backgroundCol.style.backgroundColor = 'green';
+            shuttleHeight.innerHTML = 0;
+        }
+    })
+
+    leftButton.addEventListener('click', function() {
+        movement = parseInt(rocket.style.left) - 10 + 'px';
+        rocket.style.left = movement;
+    })
+
+    rightButton.addEventListener('click', function() {
+        movement = parseInt(rocket.style.left) + 10 + 'px';
+        rocket.style.left = movement;
+    })
+
+
+    downButton.addEventListener('click', function () {
+        if (shuttleHeight.innerHTML > 0) {
+            movement = parseInt(rocket.style.bottom) - 10 + 'px';
+            rocket.style.bottom = movement;
+            shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) - 10000;
+        }
+    });
+    
+    upButton.addEventListener('click', function () {
+        if (shuttleHeight.innerHTML < 250000){
+            movement = parseInt(rocket.style.bottom) + 10 + 'px';
+            rocket.style.bottom = movement;
+            shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) + 10000;
+        }
+    });
+    
+}
+
+ window.onload = init;
+
+//  let down = this.document.getElementById('down');
+//     down.addEventListener("click", function () {
+//         movement = parseInt(imgObj.style.bottom) - 10 + 'px';
+//         imgObj.style.bottom = movement;
+//         shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) - 10000;
+//     });
